ed0e0bb7606e06c7865e5b57a5221a8c64324c1e
proximaCent <sabikaftahee01@gmail.com>
2024-10-19 21:40:34 +0600

    Hellow world with workers

--- Hello_World.cu
+++ Hello_World.cu
@@ -1,20 +1,37 @@
 #include <stdio.h>
+#include <cuda_runtime.h>
 
+#define N 16 // thread
 
-__global__ void helloFromGPU()
+// CUDA kernel
+__global__ void helloFromGPU(int *array)
 {
-    printf("Hello World from the GPU!\n");
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+
+    printf("Hello World in thread %d in block %d. Array value: %d\n", threadIdx.x, blockIdx.x, array[idx]);
 }
 
 int main()
 {
-   
-    printf("Hello World from the CPU!\n");
 
-    //launch kernel
-    helloFromGPU<<<1, 1>>>();
-    
+    int threadsPerBlock = 4;
+    int numBlocks = 3;
+
+    int h_array[N] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+    int *d_array;
+
+    // allocate memory
+    cudaMalloc((void **)&d_array, N * sizeof(int));
+
+    // Copy data from host to device
+    cudaMemcpy(d_array, h_array, N * sizeof(int), cudaMemcpyHostToDevice);
+
+    helloFromGPU<<<numBlocks, threadsPerBlock>>>(d_array);
+
     cudaDeviceSynchronize();
+
+    cudaFree(d_array);
 
     return 0;
 }
